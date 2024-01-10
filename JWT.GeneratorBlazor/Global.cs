b2c5bd9a5fd8ca6703e5df25a3ecc8b8e6ad345d
igorbdaluz <igorbdaluz@hotmail.com>
2024-01-09 23:58:50 -0300

    Adicionando Layout da biblioteca Blazor.Bootstrap

--- JWT.GeneratorBlazor/Global.cs
+++ JWT.GeneratorBlazor/Global.cs
@@ -7,6 +7,7 @@ global using JWT.GeneratorLib.Models;
 global using Microsoft.AspNetCore.Components.Web;
 global using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 global using Microsoft.AspNetCore.Components;
+global using Microsoft.AspNetCore.Components.Routing;
 
 global using System.Text.Json;
 global using System.Text;
---
+++ JWT.GeneratorBlazor/Layout/MainLayout.razor.cs
@@ -0,0 +1,25 @@
+﻿namespace JWT.GeneratorBlazor.Layout;
+
+public partial class MainLayout
+{
+    private Sidebar sidebar = default!;
+    private IEnumerable<NavItem> navItems = default!;
+
+    private async Task<SidebarDataProviderResult> SidebarDataProvider(SidebarDataProviderRequest request)
+    {
+        if (navItems is null)
+            navItems = GetNavItems();
+
+        return await Task.FromResult(request.ApplyTo(navItems));
+    }
+
+    private IEnumerable<NavItem> GetNavItems()
+    {
+        navItems = new List<NavItem>
+        {
+            new NavItem { Id = "1", Href = "/", IconName = IconName.HouseDoorFill, Text = "Home", Match=NavLinkMatch.All},
+        };
+
+        return navItems;
+    }
+}
